20841add0dc468cc3931fe2bb101d86a8d24f9f4
Alexander Chythlook <achythlook@gmail.com>
2018-09-20 10:29:25 -0800

    Begin making siteopts gain shape.

--- src/blaksite/core.clj
+++ src/blaksite/core.clj
@@ -2,7 +2,16 @@
   (:require [net.cgrand.enlive-html :as html]
             [clojure.edn :as edn]
             [clojure.string :as str]))
+
+(def locations
+  {:template "template/"
+   :output   "docs/"
+   :front-page "index.html"})
+   
+   
  
+(defn location-of [thing-name])
+
 (def thingi "woot") 
 
 (defn cleanstring [stringer]
@@ -14,24 +23,24 @@
     [ctxt]
     [:title (html/content (:blaksite/title ctxt))])
 
-(defn index-template [ctxmap]
-   (let [template (html/html-resource (java.io.StringReader. (slurp "template/html/index.html")))]
+(defmulti site-template-for identity)
+
+(defmethod site-template-for :index [ctxmap]
+   (let [template (html/html-resource (java.io.StringReader. (slurp "template/index.html")))]
      (html/at template
               [:title] (html/content (:blaksite/title ctxmap))
-              [:#pagetitle] (html/content (:blaksite/name ctxmap))
-              [:#pagesubtitle] (html/content (:blaksite/tagline ctxmap)))))
+              [:.sitetitle] (html/content (:blaksite/name ctxmap))
+              [:.sitetagline] (html/content (:blaksite/tagline ctxmap)))))
 
-(defn testme []
-  (reduce str (html/emit* (index-template (edn/read-string (slurp "siteopts.edn"))))))  
+(defn build-template [])
 
 (defn -main [& args]
   (let [opts (edn/read-string (slurp "siteopts.edn"))
-        htmres (html/html-resource (java.io.StringReader. (slurp "template/html/index.html")))]
+        htmres (html/html-resource (java.io.StringReader. (slurp "template/index.html")))]
              
     ;(println (clojure.java.io/file "template/html/index.html")) 
     ;(println)
-    (println (testme))
     (spit "blbl.html" (reduce str
                        (html/emit*
-                        (index-template opts))))))
+                        (site-template-for :index opts))))))
     ;(println htmres)))
